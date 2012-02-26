0370993569e4209c75f10a1653aaf7147eadc2e8
Neil Soni <nsoni104@gmail.com>
2012-02-26 00:58:38 -0500

    Add a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,6 +44,20 @@ describe "StaticPages" do
     end
   end
   
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+      visit  '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+  
   describe "Title Page" do
     it "should have the right title" do
       visit '/static_pages/home'
